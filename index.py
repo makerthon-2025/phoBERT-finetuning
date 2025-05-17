82b7e894c2f25d445a8fbcfddea9297f95d3333c
TKira4 <toan01224551919@gmail.com>
2025-05-17 13:16:50 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,20 @@
-arr = ['AI & Internet','An ninh mạng','An sinh','Bóng đá','Bất động sản'
- 'Chuyện của tôi','Chuyện nghề','Chính sách','Chính trị','Chợ online'
- 'Chứng khoán','Công nghệ','Công sở','Cộng đồng','Cộng đồng xe'
- 'Doanh nghiệp','Du học','Du lịch','Dự án','Gia dụng thông minh'
- 'Gia đình','Giao thông','Giáo dục','Giáo dục - Nghề nghiệp','Giải trí'
- 'Giới trẻ','Góc phụ huynh','Gương sáng','Hậu trường','Học tập Bác'
- 'Hồ sơ vụ án','Khoa học','Khoa học & đời sống','Khuyến học','Khám phá'
- 'Khỏe đẹp','Khởi nghiệp','Kinh doanh','Kinh nghiệm - Tư vấn'
- 'Kiến thức giới tính','Kiều bào','Kỷ nguyên mới','Làm giàu'
- 'Món ngon - Điểm đẹp','Môi trường','Mỹ thuật - Sân khấu'
- 'Ngoại thần kinh - Cột sống','Nhà đất','Nhân lực mới','Nhịp sống đô thị'
- 'Nóng trên mạng','Nội thất','Nội vụ','Pháp đình','Phân tích - Bình luận'
- 'Pickleball','Quân sự','Sách hay','Sản phẩm & Cộng đồng','Sống khỏe'
- 'Sống xanh','Sức khỏe','Tennis','Thượng lưu','Thế giới'
- 'Thế giới tự nhiên','Thế giới đó đây','Thể thao','Thị trường'
- 'Thị trường xe','Thời trang','Tin tức','Tiêu dùng','Tiền lương'
- 'Tour hay - Khuyến mại','Tuyển sinh','Tài chính','Tình yêu','Tư vấn'
- 'Tổ chức bộ máy','Ung thư','Video - Ảnh','Việc làm'
- 'Võ thuật - Các môn khác','Vũ trụ','Vượt lên số phận','Xe ++','Xe điện'
+arr = ['AI & Internet','An ninh mạng','An sinh','Bóng đá','Bất động sản',
+ 'Chuyện của tôi','Chuyện nghề','Chính sách','Chính trị','Chợ online',
+ 'Chứng khoán','Công nghệ','Công sở','Cộng đồng','Cộng đồng xe',
+ 'Doanh nghiệp','Du học','Du lịch','Dự án','Gia dụng thông minh',
+ 'Gia đình','Giao thông','Giáo dục','Giáo dục - Nghề nghiệp','Giải trí',
+ 'Giới trẻ','Góc phụ huynh','Gương sáng','Hậu trường','Học tập Bác',
+ 'Hồ sơ vụ án','Khoa học','Khoa học & đời sống','Khuyến học','Khám phá',
+ 'Khỏe đẹp','Khởi nghiệp','Kinh doanh','Kinh nghiệm - Tư vấn',
+ 'Kiến thức giới tính','Kiều bào','Kỷ nguyên mới','Làm giàu',
+ 'Món ngon - Điểm đẹp','Môi trường','Mỹ thuật - Sân khấu',
+ 'Ngoại thần kinh - Cột sống','Nhà đất','Nhân lực mới','Nhịp sống đô thị',
+ 'Nóng trên mạng','Nội thất','Nội vụ','Pháp đình','Phân tích - Bình luận',
+ 'Pickleball','Quân sự','Sách hay','Sản phẩm & Cộng đồng','Sống khỏe',
+ 'Sống xanh','Sức khỏe','Tennis','Thượng lưu','Thế giới',
+ 'Thế giới tự nhiên','Thế giới đó đây','Thể thao','Thị trường',
+ 'Thị trường xe','Thời trang','Tin tức','Tiêu dùng','Tiền lương',
+ 'Tour hay - Khuyến mại','Tuyển sinh','Tài chính','Tình yêu','Tư vấn',
+ 'Tổ chức bộ máy','Ung thư','Video - Ảnh','Việc làm',
+ 'Võ thuật - Các môn khác','Vũ trụ','Vượt lên số phận','Xe ++','Xe điện',
  'Xã hội','Âm nhạc','Điện ảnh','Đánh giá','Đời sống']
